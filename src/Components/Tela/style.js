1901803bcc7544cb35adb9a4479fbc236a5e90ec
Tayanne <tayh.snovaes@gmail.com>
2022-05-28 14:48:02 -0300

    feat different colors to users & fix styles and names

--- src/Components/Tela/style.js
+++ src/Components/Tela/style.js
@@ -3,48 +3,104 @@ import background from '../images/background.png';
 
 const Container = styled.div`
    box-sizing: border-box;
-   width: clamp(500px, 50vw, 50%);
+   width: clamp(500px, 50vw, 700px);
    height: 100vh;
    margin: 0 auto;
    border: solid 1px black;
    display: flex;
    flex-direction: column;
+   background-image: url(${background});
 `;
 
 const Input = styled.div`
-   display: flex;
    width: 100%;
+   display: flex;
+   justify-content: space-around;
+   margin-bottom: 0.6em;
 `;
 
-const InputNome = styled.input`
-   width: 25%;
-   height: 25px;
+const InputUser = styled.input`
+   width: 20%;
+   border: none;
    border-radius: 5px;
    padding-left: 5px;
 `;
 
-const InputMensagem = styled.input`
-   width: 75%;
+const InputMessage = styled.input`
+   width: 60%;
    border-radius: 5px;
    padding-left: 5px;
+   border: none;
+`;
+
+const Button = styled.button`
+   width: 10%;
+   padding: 0.5em;
+   border: none;
+   border-radius: 5px;
+   background-color: rgb(50, 205, 50, 0.9);
+   cursor: pointer;
 `;
 
 const Output = styled.div`
    width: 100%;
    height: 100%;
-   background-image: url(${background});
+
    display: flex;
    flex-direction: column;
    justify-content: flex-end;
 `;
 
-const OutputMensagem = styled.div`
-   width: max-content;
-   heigth: fit-content;
-   padding: 5px;
-   background-color: white;
-   border: 1px solid;
-   border-radius: 10px;
+const OutputUser = styled.div`
+   color: #008000;
+   font-size: 1em;
+   font-weight: 600;
+   margin-bottom: 0.2em;
 `;
 
-export { Container, Input, InputMensagem, InputNome, Output, OutputMensagem };
+const OutputMessage = styled.div`
+   max-width: 60%;
+   min-width: 10%;
+   heigth: fit-content;
+   padding: 10px;
+   margin-bottom: 1em;
+   border-style: none;
+   border-radius: 0.5em;
+   line-height: 1.3;
+   font-weight: 450;
+   box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
+   word-wrap: break-word;
+
+   background-color: ${(props) =>
+      props.user === 'eu' ? '#DDF7C8' : '#ffffff'};
+
+   align-self: ${(props) => (props.user === 'eu' ? 'flex-end' : 'flex-start')};
+
+   margin-right: ${(props) => (props.user === 'eu' ? '1em' : '')};
+
+   margin-left: ${(props) => (props.user !== 'eu' ? '1em' : '')};
+
+   position: relative;
+   &:after {
+      content: '';
+      border: 15px solid transparent;
+      border-top-color: ${(props) =>
+         props.user === 'eu' ? '#DDF7C8' : '#ffffff'};
+
+      position: absolute;
+      top: 0px;
+      right: ${(props) => (props.user === 'eu' ? '-8px' : '')};
+      left: ${(props) => (props.user !== 'eu' ? '-8px' : '')};
+   }
+`;
+
+export {
+   Container,
+   Input,
+   InputMessage,
+   InputUser,
+   Button,
+   Output,
+   OutputUser,
+   OutputMessage,
+};
